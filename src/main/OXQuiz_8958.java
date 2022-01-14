8de42e1c8904d615e4809bdd75907403bcfdef9c
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-01-14 13:53:00 +0900

    입출력: OX퀴즈_8958

---
+++ src/main/OXQuiz_8958.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class OXQuiz_8958 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			List<String> list = Arrays.asList(br.readLine().split("X"));
+			printScore(list);
+		}
+	}
+
+	private static void printScore(List<String> list) {
+		int score = 0;
+		for (String os : list) {
+			score += sum(os.length());
+		}
+		System.out.println(score);
+	}
+
+	private static int sum(int num) {
+		return num * (num + 1) / 2;
+	}
+}
