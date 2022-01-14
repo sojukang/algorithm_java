aa0e37e37d0b94bcb8453444be809b840af3943b
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-01-14 13:06:17 +0900

    입출력: 평균_1546

---
+++ src/main/Mean_1546.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Mean_1546 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		List<Integer> integers = getIntegersBySpace(br);
+
+		int max = Collections.max(integers);
+		int sum = getSum(integers);
+		double newAvg = (double) sum / (max * N) * 100;
+		System.out.println(newAvg);
+	}
+
+	private static int getSum(List<Integer> integers) {
+		return integers.stream().mapToInt(i -> i).sum();
+	}
+
+	private static List<Integer> getIntegersBySpace(BufferedReader br) throws IOException {
+		List<String> strings = Arrays.asList(br.readLine().split(" "));
+		List<Integer> integers = strings.stream().map(Integer::parseInt).collect(Collectors.toList());
+		return integers;
+	}
+}
