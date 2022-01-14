c26fa7331028b10446d9528fbdf7b4628755f8b8
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-01-14 11:33:41 +0900

    입출력: A+B_1000

---
+++ src/main/AplusB_1000.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AplusB_1000 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] input = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int sum = Arrays.stream(input).sum();
+		System.out.print(sum);
+	}
+}
