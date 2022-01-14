a91fe265531cbd862346be56afef87c04e8b6545
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-01-14 13:05:27 +0900

    입출력: Max_2562

---
+++ src/main/Max_2562.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Max_2562 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int max = Integer.MIN_VALUE;
+		int idx = 0;
+		int num = 0;
+		for (int i = 1; i <= 9; i++) {
+			num = Integer.parseInt(br.readLine());
+			if (num > max) {
+				max = num;
+				idx = i;
+			}
+		}
+		System.out.print(max + "\n" + idx);
+	}
+}
