1f236bc6d036da0e616699859afa53baa4a9c37b
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-01-14 13:06:01 +0900

    입출력: 나머지_3052

---
+++ src/main/Remains_3052.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Remains_3052 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int iteration = 10;
+		int divisor = 42;
+		int result = noOfRemains(iteration, divisor, br);
+
+		System.out.println(result);
+	}
+
+	private static int noOfRemains(int iteration, int divisor, BufferedReader br) throws IOException {
+		int numToDivide;
+		int remain;
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 0; i < 10; i++) {
+			numToDivide = Integer.parseInt(br.readLine());
+			remain = numToDivide % 42;
+			set.add(remain);
+		}
+
+		return set.size();
+	}
+}
